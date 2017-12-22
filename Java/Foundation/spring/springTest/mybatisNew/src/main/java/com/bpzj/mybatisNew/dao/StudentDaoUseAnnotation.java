ee6ef1f4c98c610a55fcfe9536e7712c62adba58
bpzj <1521189886@qq.com>
2017-12-23 05:05:07 +0800

    测试mybatis基于接口的写法
    使用两种方式
    1 基于xml文件配置mapper
    2 基于注解配置

---
+++ Java/Foundation/spring/springTest/mybatisNew/src/main/java/com/bpzj/mybatisNew/dao/StudentDaoUseAnnotation.java
@@ -0,0 +1,35 @@
+package com.bpzj.mybatisNew.dao;
+
+import com.bpzj.mybatisNew.domain.Student;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface StudentDaoUseAnnotation {
+
+    @Insert("INSERT INTO sign VALUES(NULL,NOW(),NOW(),#{name},#{qq},#{type},#{entryTime}," +
+            "#{school},#{studentId},#{link},#{oath},#{brother},#{knowFrom})")
+    void add(Student student);
+
+    //删除
+    @Delete("DELETE FROM sign WHERE id=#{id}")
+    void deleteById(int id);
+
+    //修改
+    @Update("UPDATE sign SET update_at=NOW(), name=#{name}, type=#{type}, oath=#{oath} WHERE id=#{id}")
+    void updateById(Student student);
+
+    //获取
+    Student getById(int id);
+
+    //查询
+    @Select("SELECT * FROM sign")
+    List<Student> findAll();
+
+    //分页查询
+//    List<Student> findAll(int start, int count);
+
+}
