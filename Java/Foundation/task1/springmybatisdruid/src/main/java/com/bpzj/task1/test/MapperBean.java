2d21c79ce706d1c2cc9df81921f58e5438973a6f
bpzj <1521189886@qq.com>
2017-12-28 06:21:52 +0800

    main()测试

---
+++ Java/Foundation/task1/springmybatisdruid/src/main/java/com/bpzj/task1/test/MapperBean.java
@@ -0,0 +1,19 @@
+package com.bpzj.task1.test;
+
+import com.bpzj.task1.dao.StudentMapper;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class MapperBean {
+
+    // 实例化容器
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("bean-druid.xml");
+
+    // 获取mapper bean
+    StudentMapper studentMapper = (StudentMapper) ctx.getBean("studentMapper");
+
+    public StudentMapper getStudentMapper() {
+        return studentMapper;
+    }
+}
