a09c7c76db3056ee8bbed1437861af5aba27ca14
bpzj <1521189886@qq.com>
2017-12-28 07:08:37 +0800

    更新文件

--- Java/Easy/删除没用的文件.py
+++ Java/Easy/删除没用的文件.py
@@ -10,6 +10,12 @@ for root, dirs, files in os.walk('./'):
         if dir_name.endswith(".idea"):
             # os.remove(os.path.join(root, dir_name))
             shutil.rmtree(os.path.join(root, dir_name))
+        elif dir_name.endswith("target"):
+            shutil.rmtree(os.path.join(root, dir_name))
+        elif dir_name.endswith("log"):
+            shutil.rmtree(os.path.join(root, dir_name))
+        elif dir_name.endswith("logs"):
+            shutil.rmtree(os.path.join(root, dir_name))
 
 
 # 删除 符合规则的 文件，
