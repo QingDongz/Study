1e1ec84921e9ef50f72da6330157d4ef31689b3f
bpzj <1521189886@qq.com>
2017-12-28 06:55:39 +0800

    更新py脚本

--- Java/Easy/删除没用的文件.py
+++ Java/Easy/删除没用的文件.py
@@ -1,19 +1,13 @@
 import os
 import shutil
 
-n = 0
-
 
 # 删除 符合规则的 文件夹，
 for root, dirs, files in os.walk('./'):
     for dir_name in dirs:
-        # print(root)
         # print(dirs)
-        # print()
         # print(files)
         if dir_name.endswith(".idea"):
-            # n += 1
-            # print(n)
             # os.remove(os.path.join(root, dir_name))
             shutil.rmtree(os.path.join(root, dir_name))
 
